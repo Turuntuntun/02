020a2f81219190291a332553feb0c13071e64c99
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2019-09-27 02:03:14 +0300

    Изменена логика проверки и заполнения куки

--- index.php
+++ index.php
@@ -1,16 +1,26 @@
 <?php
-
-$info = json_decode(file_get_contents('results/result.txt'));
+//$result = [
+//    1 => [
+//        'LINKS' => [
+//            'https://dae.tk' => 'vk.com',
+//            'COUNT' => 0
+//        ]
+//    ]
+//];
+//file_put_contents('results/result.txt',json_encode($result)); die();
+$info = json_decode(file_get_contents('results/result.txt'),true);
 
 if ($_COOKIE['ID']) {
     $id = $_COOKIE['ID'];
-    $data = $info['ID'];
 } else {
     if (empty($info)) {
         $id = 1;
+
     } else {
         $id = count($info);
     };
+    $info[$id] = [];
+    file_put_contents('results/result.txt',json_encode($info));
     setcookie('ID',$id);
 }
 
