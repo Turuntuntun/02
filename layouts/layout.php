106519511b6445968ea637f757021a2e4ffe66d7
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2019-09-27 02:05:16 +0300

    Добавлен новый ajax запрос

--- layouts/layout.php
+++ layouts/layout.php
@@ -11,6 +11,9 @@
         <input class = 'link' placeholder="Введите ссылку">
         <input type="button" value="Ввести" class = 'submit'>
     </form>
+    <form>
+        <input type = button value="Показать список ссылок" class = 'list'>
+    </form>
     <div class = 'result'>
 
     </div>
@@ -19,17 +22,43 @@
     </div>
     <script>
         $(document).ready(function () {
+            function get_cookie ( cookie_name )
+            {
+                var results = document.cookie.match ( '(^|;) ?' + cookie_name + '=([^;]*)(;|$)' );
+
+                if ( results )
+                    return ( unescape ( results[2] ) );
+                else
+                    return null;
+            }
             $('.submit').on('click',function () {
-                linkVal = $('.link').val();
+                let linkVal = $('.link').val();
+                let validate = /(^https?:\/\/)?[a-z0-9~_\-\.]+\.[a-z]{2,9}(\/|:|\?[!-~]*)?$/i;
+                if (validate.test(linkVal)) {
+                    $.ajax({
+                        type: "POST",
+                        url: "/../helpers/addLink.php",
+                        data: {link:linkVal,id:get_cookie('ID')},
+                        success:function () {
+                            $('.result').html('<p>Ссылка успешно добавлена</p>');
+                        }
+                    });
+                } else {
+                    $('.result').html('<p>Некоректная ссылка, напишите корректную ссылку</p>');
+                    console.log();
+                }
+
+            });
+            $('.list').on('click',function () {
                 $.ajax({
                     type: "POST",
-                    url: "/../helpers/addLink.php",
-                    data: {link:linkVal},
+                    url: "/../helpers/showLinks.php",
+                    data: {id:get_cookie('ID')},
                     success:function () {
-                        alert(linkVal);
                     }
                 });
-            })
+
+            });
         });
     </script>
 </body>
