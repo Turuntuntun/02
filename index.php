d8043af83efb515515293d61a48904f9e66f81c3
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2019-09-27 06:05:17 +0300

    Убран мусор

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
-//$result = [
-//    1 => [
-//        'LINKS' => [
-//            'https://dae.tk' => 'vk.com',
-//            'COUNT' => 0
-//        ]
-//    ]
-//];
-//file_put_contents('results/result.txt',json_encode($result)); die();
+
 $info = json_decode(file_get_contents('results/result.txt'),true);
 
 if ($_COOKIE['ID']) {
@@ -24,5 +16,4 @@ if ($_COOKIE['ID']) {
     setcookie('ID',$id);
 }
 
-//file_put_contents('.htaccess',PHP_EOL.'Redirect /123/ http://vk.com/',FILE_APPEND);
 include 'layouts/layout.php';
